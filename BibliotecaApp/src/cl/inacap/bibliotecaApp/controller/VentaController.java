95d63575ffcffd6f52b5e167e8b7ced4168834b6
constanzaB72 <constanzabelen1.bm@hotmail.com>
2021-07-21 23:24:14 -0400

    recuperacion controllers venta

--- BibliotecaApp/src/cl/inacap/bibliotecaApp/controller/VentaController.java
+++ BibliotecaApp/src/cl/inacap/bibliotecaApp/controller/VentaController.java
@@ -2,28 +2,87 @@ package cl.inacap.bibliotecaApp.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.table.DefaultTableModel;
+
+import cl.inacap.bibliotecaApp.controller.CompraController.AgregarListener;
+import cl.inacap.bibliotecaApp.controller.CompraController.CancelarAgregarListener;
+import cl.inacap.bibliotecaApp.controller.CompraController.CompraListener;
+import cl.inacap.bibliotecaApp.controller.CompraController.SelectListener;
+import cl.inacap.bibliotecaApp.frames.SelectLibrosJIFrame;
+import cl.inacap.bibliotecaApp.frames.VentaExitosa;
 import cl.inacap.bibliotecaApp.frames.VentaFrame;
+import cl.inacap.bibliotecaModel.dao.DistribuidorDAO;
+import cl.inacap.bibliotecaModel.dao.EjemplarDAO;
+import cl.inacap.bibliotecaModel.dao.LibrosDAO;
+import cl.inacap.bibliotecaModel.dto.Distribuidor;
+import cl.inacap.bibliotecaModel.dto.Libro;
+import cl.inacap.bibliotecaModel.utils.Generador;
 
 public class VentaController {
-	VentaFrame ventaFrame;
+	private VentaFrame ventaFrame;
 	private String rut;
 	private String tipoUsuario;
+	private VentaExitosa ventaExitosa;
+	private DistribuidorDAO distribuidorDAO= new DistribuidorDAO();
+	private LibrosDAO librosDAO = new LibrosDAO();
+	private List<Libro> libros;
+	private SelectLibrosJIFrame listalibros;
+	private LibrosDAO libroDao=new LibrosDAO();
+	private EjemplarDAO ejemplarDAO=new EjemplarDAO();
+	private Generador generador =new Generador();
 	
 	public VentaController(String rut, String tipoUsuario) {
 		this.rut=rut;
 		this.tipoUsuario=tipoUsuario;
 		ventaFrame = new VentaFrame();
+		List<Distribuidor> distribuidores = distribuidorDAO.getAll();
+		libros = librosDAO.getAll();
+		
 		ventaFrame.addMenuListener(new MenuListener());
+		ventaFrame.addSelectListener(new SelectListener());
+		ventaFrame.repaint();
 	}
 	class MenuListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			if(tipoUsuario.equals("trabajador")) {
-				TrabajadorController trabajador = new TrabajadorController(rut);
-			}else {
-				ClienteController cliente = new ClienteController(rut);
-			}
+			TrabajadorController trabajador = new TrabajadorController(rut);
 			ventaFrame.dispose();
+		}
+	}
+	//class ventaListener
+	class FinalizarListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			ventaFrame.dispose();
+			ventaExitosa.setVisible(false);
+			ventaExitosa.dispose();
+		}
+	}
+	class SelectListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			listalibros =new SelectLibrosJIFrame(libros);
+			listalibros.addConfirmarAgregar(new AgregarListener());
+			listalibros.addCancelarAgregar(new CancelarAgregarListener());
+	}
+	}
+	class AgregarListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			DefaultTableModel aux=(DefaultTableModel)listalibros.getTable2().getModel();
+			ventaFrame.getTableLibros().setModel(aux);
+			ventaFrame.getTableLibros().removeColumn(ventaFrame.getTableLibros().getColumnModel().getColumn(4));
+			Integer suma=0;
+			for(int fila=0;fila<aux.getRowCount();fila++) {
+				suma=suma+((Integer) aux.getValueAt(fila, 2)/* */);				
+			}
+			ventaFrame.getTxtCosto().setText("$"+String.valueOf(suma));
+			listalibros.dispose();
+			
+		}
+	}
+	class CancelarAgregarListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			listalibros.dispose();
+			
 		}
 	}
 	
---
+++ BibliotecaApp/src/cl/inacap/bibliotecaApp/frames/VentaExitosa.java
@@ -0,0 +1,53 @@
+package cl.inacap.bibliotecaApp.frames;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+
+public class VentaExitosa extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JButton btnAceptar; 
+	private JLabel lblAlerta;
+
+	public VentaExitosa() {
+		getContentPane().setLayout(null);
+		
+		lblAlerta = new JLabel("Arriendo registrado Exitosamente!");
+		lblAlerta.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblAlerta.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAlerta.setBounds(0, 120, 436, 14);
+		getContentPane().add(lblAlerta);
+		
+		setBounds(100, 100, 400, 300);
+		getContentPane().setLayout(null);
+		contentPanel.setBounds(0, 0, 0, 0);
+		contentPanel.setLayout(new FlowLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBounds(0, 253, 386, 10);
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane);
+			{
+
+				btnAceptar = new JButton("ACEPTAR");
+				btnAceptar.setBounds(170, 145, 89, 23);
+				getContentPane().add(btnAceptar);
+			
+			}}
+	}
+	
+	public void addFinalizarListener(ActionListener ing) {
+		btnAceptar.addActionListener(ing);
+	}
+}
